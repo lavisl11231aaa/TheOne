485c4f4567449b7a4ac8018031d578c5f24a22fe
lavisl11231aaa <124031753+lavisl11231aaa@users.noreply.github.com>
2023-01-31 15:45:53 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from support import ui_elements as ui
+from data import userinfo as u
